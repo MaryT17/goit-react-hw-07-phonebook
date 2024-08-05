3af3d2255ec790f5cf18905f9cfaf052337e58b4
MaryT17 <tmary11996@gmail.com>
2024-08-05 14:41:24 +0800

    update3

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,26 @@
 import { combineReducers } from 'redux';
-import { contactsReducer, filterReducer } from './reducers';
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { filterSlice } from './filterSlice';
+import { contactsSlice } from './contactsSlice';
 
+// Combine reducers
 const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer,
+  contacts: contactsSlice.reducer,
+  filter: filterSlice.reducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['contacts'], 
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
